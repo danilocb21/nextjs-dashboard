f2a0a2b8abe4898fa9cbce032855a2806524700c
Danilo <danilocbjp@hotmail.com>
2024-10-17 19:24:38 -0300

    chapter 15, 16 and customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,18 @@
-const Page = () => {
-  return <p>Customers Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
 };
 
-export default Page;
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+
+  return <CustomersTable query={query} />;
+}
